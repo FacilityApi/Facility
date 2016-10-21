279fc0500071a2cdc3939a5cf903e8438a2195ef
Ed Ball <ed.ball@logos.com>
2016-10-20 22:14:34 -0700

    Add capitalize method.

---
+++ tests/Facility.Definition.UnitTests/CodeGen/CodeGenUtilityTests.cs
@@ -0,0 +1,27 @@
+﻿using Facility.Definition.CodeGen;
+using Shouldly;
+using Xunit;
+
+namespace Facility.Definition.UnitTests.CodeGen
+{
+	public sealed class CodeGenUtilityTests
+	{
+		[Fact]
+		public void CapitalizeLowerCase()
+		{
+			CodeGenUtility.Capitalize("xyzzy").ShouldBe("Xyzzy");
+		}
+
+		[Fact]
+		public void CapitalizeUpperCase()
+		{
+			CodeGenUtility.Capitalize("Xyzzy").ShouldBe("Xyzzy");
+		}
+
+		[Fact]
+		public void CapitalizeNumber()
+		{
+			CodeGenUtility.Capitalize("1234").ShouldBe("1234");
+		}
+	}
+}
