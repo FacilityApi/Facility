106365f2e83cc999e0d83fac7d4eb6f3e0960c0d
Ed Ball <ejball@gmail.com>
2018-04-28 16:04:16 -0700

    Add CodeGenUtilityTests.

--- tests/Facility.Definition.UnitTests/CodeGen/CodeGenUtilityTests.cs
+++ tests/Facility.Definition.UnitTests/CodeGen/CodeGenUtilityTests.cs
@@ -6,22 +6,53 @@ namespace Facility.Definition.UnitTests.CodeGen
 {
 	public sealed class CodeGenUtilityTests
 	{
-		[Test]
-		public void CapitalizeLowerCase()
+		[TestCase("xyZzy", "XyZzy")]
+		[TestCase("XyZzy", "XyZzy")]
+		[TestCase("1234", "1234")]
+		public void Capitalize(string before, string after)
 		{
-			CodeGenUtility.Capitalize("xyzzy").Should().Be("Xyzzy");
+			CodeGenUtility.Capitalize(before).Should().Be(after);
 		}
 
-		[Test]
-		public void CapitalizeUpperCase()
+		[TestCase("xyZzy", "xyZzy")]
+		[TestCase("XyZzy", "xyZzy")]
+		[TestCase("1234", "1234")]
+		public void Uncapitalize(string before, string after)
 		{
-			CodeGenUtility.Capitalize("Xyzzy").Should().Be("Xyzzy");
+			CodeGenUtility.Uncapitalize(before).Should().Be(after);
 		}
 
-		[Test]
-		public void CapitalizeNumber()
+		[TestCase("xyZzy", "xyZzy")]
+		[TestCase("XyZzy", "xyZzy")]
+		[TestCase("1234", "1234")]
+		[TestCase("xy_zzy", "xyZzy")]
+		[TestCase("me2you", "me2You")]
+		[TestCase("IOStream", "ioStream")]
+		public void CamelCase(string before, string after)
 		{
-			CodeGenUtility.Capitalize("1234").Should().Be("1234");
+			CodeGenUtility.ToCamelCase(before).Should().Be(after);
+		}
+
+		[TestCase("xyZzy", "XyZzy")]
+		[TestCase("XyZzy", "XyZzy")]
+		[TestCase("1234", "1234")]
+		[TestCase("xy_zzy", "XyZzy")]
+		[TestCase("me2you", "Me2You")]
+		[TestCase("IOStream", "IOStream")]
+		public void PascalCase(string before, string after)
+		{
+			CodeGenUtility.ToPascalCase(before).Should().Be(after);
+		}
+
+		[TestCase("xyZzy", "xy_zzy")]
+		[TestCase("XyZzy", "xy_zzy")]
+		[TestCase("1234", "1234")]
+		[TestCase("xy_zzy", "xy_zzy")]
+		[TestCase("me2you", "me_2_you")]
+		[TestCase("IOStream", "io_stream")]
+		public void SnakeCase(string before, string after)
+		{
+			CodeGenUtility.ToSnakeCase(before).Should().Be(after);
 		}
 	}
 }
