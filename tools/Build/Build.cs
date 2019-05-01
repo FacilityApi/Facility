b834474205793ee203233f47b5265d1c7bd7c45c
Ed Ball <ed.ball@faithlife.com>
2019-05-01 16:23:59 -0700

    Add target descriptions.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -31,10 +31,12 @@ internal static class Build
 
 		build.Target("codegen")
 			.DependsOn("build")
+			.Describe("Generates code from the FSD")
 			.Does(() => codeGen(verify: false));
 
 		build.Target("verify-codegen")
 			.DependsOn("build")
+			.Describe("Ensures the generated code is up-to-date")
 			.Does(() => codeGen(verify: true));
 
 		build.Target("test")
